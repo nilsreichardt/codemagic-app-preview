362fda4e3d456ba2569f135136139b2015b1af92
actranito <actranito@gmail.com>
2024-12-20 19:59:09 +0000

    fix(codemagic_app_preview): fix posting comment on GitLab (#111)
    The Gitlab comment was getting cut off on the first `&` symbol in the
    query param of the `qrCodeUrl`.
    
    <img width="890" alt="Screenshot 2024-12-20 at 14 58 17"
    src="https://github.com/user-attachments/assets/0ca6daff-82d8-41ab-ba7a-cea119ad6e14"
    />
    
    This was happening since the Gitlab API takes in the body of the comment
    as a query param as well, so the api was parsing the `&` as another
    query param on the endpoint call.
    The proposed fix encodes the comment body first before calling the API.
    
    ---------
    
    Co-authored-by: Andre Ranito <andre.ranito@knowunity.com>
    Co-authored-by: Nils Reichardt <me@nils.re>

--- packages/codemagic_app_preview/lib/src/git/gitlab_api_repository.dart
+++ packages/codemagic_app_preview/lib/src/git/gitlab_api_repository.dart
@@ -38,9 +38,10 @@ class GitLabApiRepository implements GitHostRepository {
   Future<PostedComment> postComment(
     String comment,
   ) async {
+    final encodedComment = Uri.encodeComponent(comment);
     final response = await httpClient.post(
       Uri.parse(
-        '$_baseUrl/api/v4/projects/$projectId/merge_requests/$mergeRequestId/notes?body=$comment',
+        '$_baseUrl/api/v4/projects/$projectId/merge_requests/$mergeRequestId/notes?body=$encodedComment',
       ),
       headers: {
         'Authorization': 'Bearer $token',
@@ -59,9 +60,10 @@ class GitLabApiRepository implements GitHostRepository {
   /// Edits an existing comment.
   @override
   Future<void> editComment(int commentId, String comment) async {
+    final encodedComment = Uri.encodeComponent(comment);
     final response = await httpClient.put(
       Uri.parse(
-        '$_baseUrl/api/v4/projects/$projectId/merge_requests/$mergeRequestId/notes/$commentId?body=$comment',
+        '$_baseUrl/api/v4/projects/$projectId/merge_requests/$mergeRequestId/notes/$commentId?body=$encodedComment',
       ),
       headers: {
         'Authorization': 'Bearer $token',
